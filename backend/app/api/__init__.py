20537b0cae009e80aaea3491adc5bae92ce123dd
Evandro Araújo Luz <devealuz@gmail.com>
2026-02-27 16:29:23 -0300

    chore: add metrics route

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,10 +1,11 @@
 """
 API module.
 """
-from app.api.routes import health_router, images_router, batches_router
+from app.api.routes import health_router, images_router, batches_router, metrics_router
 
 __all__ = [
     "health_router",
     "images_router", 
     "batches_router",
+    "metrics_router",
 ]
